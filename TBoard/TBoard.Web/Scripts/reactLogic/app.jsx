227b900432b36c4ec547933e48dcd8359f29b476
Renaldobenji <Renaldobenji@gmail.com>
2016-05-22 15:28:58 +0200

    Private Buy Registration UI Complete
    Private Buy Registration UI Complete

--- TBoard/TBoard.Web/Scripts/reactLogic/app.jsx
+++ TBoard/TBoard.Web/Scripts/reactLogic/app.jsx
@@ -36,13 +36,169 @@
   }
 });
 
-var register = React.createClass({	
+/*Register Page*/
+var RegisterType = React.createClass({	
 	render: function(){
-		return (
-			<h1>This is the register Page</h1>
+		return (			
+				<div className="panel panel-primary">
+					<div className="panel-heading">
+						Registration Type
+					</div>
+					<div className="panel-body">
+						<form role="form">
+							<div className="form-group">
+								<div className="radio">
+									<label>
+										<input type="radio" name="registrationOptions" id="registrationOptionsPrivateBuyer" value="Private Buyer" checked/> Private Buyer
+									</label>
+								</div>
+								<div className="radio">
+									<label>
+										<input type="radio" name="registrationOptions" id="registrationOptionsCorporateBuyer" value="Corporate Buyer"/> Corporate Buyer
+									</label>
+								</div>
+								<div className="radio">
+									<label>
+										<input type="radio" name="registrationOptions" id="registrationCorporateSeller" value="Corporate Seller"/> Corporate Seller
+									</label>
+								</div>
+							</div>
+						</form>
+					</div>						
+				</div>				
 		);
 	}
 });
+
+/*Register Page*/
+var Register = React.createClass({	
+	render: function(){
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-md-4">
+						<RegisterType />
+					</div>					
+					<div className="col-md-8">
+						<RegisterPrivate />
+						<RegisterContactDetails />
+						<RegisterAddress />						
+					</div>
+				</div>							
+			</div>
+		);
+	}
+});
+
+
+var RegisterPrivate = React.createClass({	
+	render: function(){
+		return (
+			<div className="panel panel-primary">
+				<div className="panel-heading">
+					Private Buyer
+				</div>
+				<div className="panel-body">
+					<form>
+						<div className="form-group">
+                            <label>Name</label>
+                            <input id="Name" className="form-control" placeholder="Name"/>
+                        </div>
+						<div className="form-group">
+                            <label>Surname</label>
+                            <input id="Surname" className="form-control" placeholder="Surname"/>
+                        </div>
+						<div className="form-group">
+                            <label>Password</label>
+                            <input id="Password" className="form-control" placeholder="Password"/>
+                        </div>
+						<div className="form-group">
+                            <label>Confirm Password</label>
+                            <input id="ConfirmPassword" className="form-control" placeholder="Confirm Password"/>
+                        </div>
+						<div className="form-group">
+                            <label>ID Number</label>
+                            <input id="IDNumber" className="form-control" placeholder="ID Number"/>
+                        </div>
+					</form>
+				</div>						
+			</div>
+		);
+	}
+});
+
+var RegisterContactDetails = React.createClass({	
+	render: function(){
+		return (
+			<div className="panel panel-primary">
+				<div className="panel-heading">
+					Contact Information
+				</div>
+				<div className="panel-body">
+					<form>
+						<div className="form-group">
+                            <label>Cell Number</label>
+                            <input id="CellNumber" className="form-control" placeholder="Cell Number"/>
+                        </div>
+						<div className="form-group">
+                            <label>Home Number</label>
+                            <input id="HomeNumber" className="form-control" placeholder="Home Number"/>
+                        </div>
+						<div className="form-group">
+                            <label>Work Number</label>
+                            <input id="WorkNumber" className="form-control" placeholder="Work Number"/>
+                        </div>
+						<div className="form-group">
+                            <label>Email</label>
+                            <input id="Email" className="form-control" placeholder="Email"/>
+                        </div>						
+					</form>
+				</div>						
+			</div>
+		);
+	}
+});
+
+var RegisterAddress = React.createClass({	
+	render: function(){
+		return (
+			<div className="panel panel-primary">
+				<div className="panel-heading">
+					Address Information
+				</div>
+				<div className="panel-body">
+					<form>
+						<div className="form-group">
+                            <label>Address Line 1</label>
+                            <input id="AddressLine1" className="form-control" placeholder="Address Line 1"/>
+                        </div>
+						<div className="form-group">
+                            <label>Address Line 2</label>
+                            <input id="AddressLine2" className="form-control" placeholder="Address Line 2"/>
+                        </div>
+						<div className="form-group">
+                            <label>Address Line 3</label>
+                            <input id="AddressLine3" className="form-control" placeholder="Address Line 3"/>
+                        </div>
+						<div className="form-group">
+                            <label>Address Line 4</label>
+                            <input id="AddressLine4" className="form-control" placeholder="Address Line 4"/>
+                        </div>
+						<div className="form-group">
+                            <label>Address Line 5</label>
+                            <input id="AddressLine5" className="form-control" placeholder="Address Line 5"/>
+                        </div>
+						<div className="form-group">
+                            <label>Postal Code</label>
+                            <input id="PostalCode" className="form-control" placeholder="Postal Code"/>
+                        </div>
+					</form>
+				</div>						
+			</div>
+		);
+	}
+});
+
 
 routie({
 		'': function(){
@@ -52,7 +208,7 @@ routie({
 		},
 		'register': function(){
 			ReactDOM.render(
-				React.createElement(register, null), document.getElementById('container')
+				React.createElement(Register, null), document.getElementById('container')
 			);
 		}	
 });
