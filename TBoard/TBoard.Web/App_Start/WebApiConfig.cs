fec248e03676d46fa7767c8a220f15754314b4bf
Renaldobenji <Renaldobenji@gmail.com>
2016-09-03 04:07:10 -0700

    Elmah Added in for error handling
    Elmah Added in for error handling

--- TBoard/TBoard.Web/App_Start/WebApiConfig.cs
+++ TBoard/TBoard.Web/App_Start/WebApiConfig.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Elmah.Contrib.WebApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using TBoard.Web.Services;
 
 namespace TBoard.Web
 {
@@ -19,6 +22,10 @@ namespace TBoard.Web
 
             var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
             config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+            config.Services.Add(typeof(IExceptionLogger), new ElmahExceptionLogger());
+            //config.Filters.Add(new UnhandledExceptionFilter());
+
         }
     }
 }
