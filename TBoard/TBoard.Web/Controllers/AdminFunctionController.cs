c82d7a0541b73134a0aef327dd55155ec6024fb0
Renaldobenji <Renaldobenji@gmail.com>
2018-08-11 11:43:40 +0200

    created admin function
    created admin function

---
+++ TBoard/TBoard.Web/Controllers/AdminFunctionController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using TBoard.Web.Models;
+using TBoard.BusinessLogic.BusinessLogic;
+
+namespace TBoard.Web.Controllers
+{
+    public class AdminFunctionController : Controller
+    {
+        private List<VerifyDocuments> verifyDocuments;
+
+        private DocumentRequirementBusinessLogic documentRequirementBusinessLogic;
+        private DocumentBusinessLogic documentBusinessLogic;
+        ConfigBusinessLogic configBusinesslogic;
+        public AdminFunctionController(DocumentRequirementBusinessLogic documentRequirementBusinessLogic, DocumentBusinessLogic documentBusinessLogic, ConfigBusinessLogic configBusinesslogic)
+        {
+            this.documentRequirementBusinessLogic = documentRequirementBusinessLogic;
+            this.documentBusinessLogic = documentBusinessLogic;
+            this.configBusinesslogic = configBusinesslogic;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public ActionResult VerifyDocuments()
+        {
+            verifyDocuments = new List<VerifyDocuments>()
+            {
+                new Models.VerifyDocuments()
+                {
+                    DocumentType = "DocumentType",
+                    DocumentURL = "DocumentURL",
+                    OrganizationID = 1,
+                    OrganizationName = "OrganizationName",
+                    Verified  = true
+                },
+                new Models.VerifyDocuments()
+                {
+                    DocumentType = "DocumentURL",
+                    DocumentURL = "DocumentType",
+                    OrganizationID = 1,
+                    OrganizationName = "OrganizationName",
+                    Verified  = false
+                }
+            };
+
+            return View(verifyDocuments);
+        }
+    }
+}
---
+++ TBoard/TBoard.Web/Models/VerifyDocuments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TBoard.Web.Models
+{
+    public class VerifyDocuments
+    {
+
+        public int OrganizationID { get; set; }
+        public string DocumentType { get; set; }
+        public string DocumentURL { get; set; }
+        public string OrganizationName { get; set; }
+        public Boolean Verified { get; set; }
+        
+
+    }
+}
