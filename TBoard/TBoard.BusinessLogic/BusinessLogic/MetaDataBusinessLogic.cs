3a3dbdb2ca9d3cb5fe6fdd56e332c4b3226cb551
Renaldobenji <Renaldobenji@gmail.com>
2018-10-02 19:23:18 +0200

    Meta Data Scoring system
    Meta Data Scoring system

--- TBoard/TBoard.BusinessLogic/BusinessLogic/MetaDataBusinessLogic.cs
+++ TBoard/TBoard.BusinessLogic/BusinessLogic/MetaDataBusinessLogic.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using TBoard.Data.Interfaces;
 using TBoard.Data.Model;
 using TBoard.Data.Repository;
@@ -28,8 +30,32 @@ namespace TBoard.BusinessLogic.BusinessLogic
     
         public int MetaDataScoringSystem(FetchMetaData weightingMetaDataToFetch, FetchMetaData ratingMetaData)
         {
-            var weightingJSON = this.GetMetaDataByMetaDataName(weightingMetaDataToFetch);
+            var weightingJSON = this.GetMetaDataByMetaDataName(weightingMetaDataToFetch).FirstOrDefault();
             var ratingMetaDataValues = this.GetMetaDataByMetaDataName(ratingMetaData);
+
+            int total = 0;
+            JObject weighting = JObject.Parse(weightingJSON.metaDataValue);
+
+            foreach (var value in ratingMetaDataValues)
+            {
+                string jsonMetaDataValue = (string)weighting[value.metaDataName];
+                if (!string.IsNullOrEmpty(jsonMetaDataValue))
+                {
+                    var multipleScoringFacets = jsonMetaDataValue.Split(',');
+                    foreach (var multiple in multipleScoringFacets)
+                    {
+                        var score = multiple.Split('$');
+                        if (score[0] == value.metaDataValue)
+                        {
+                            total += Convert.ToInt32(score[1]);
+                        }
+                    }
+                   
+                }
+            }
+
+            return total;
+
         }
 
     }
